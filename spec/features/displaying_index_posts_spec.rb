27314613994dc6a3511457d0dcfa61d7fd1c08cf
Steven Abrams <steven.abrams86@gmail.com>
2016-05-28 23:25:39 -0600

    index shows posts

--- spec/features/displaying_index_posts_spec.rb
+++ spec/features/displaying_index_posts_spec.rb
@@ -1,1 +1,12 @@
 require 'rails_helper.rb'
+
+feature 'Index displays a list of posts' do
+  scenario 'the index displays correct created job information' do
+    job_one = create(:post, caption: "This is post one")
+    job_two = create(:post, caption: "This is post two")
+    visit '/'
+    expect(page).to have_content("This is post one")
+    expect(page).to have_content("This is post two")
+    expect(page).to have_css("img[src*='coffee']")
+  end
+end
