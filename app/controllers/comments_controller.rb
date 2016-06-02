e4b8082d8cc8282ad550363e2a0aac64aea73a93
Steven Abrams <steven.abrams86@gmail.com>
2016-06-01 20:55:21 -0600

    add ajax, but it's not fully working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,27 +10,25 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
 
     if @comment.save
-      flash[:success] = "You commented the hell out of that post!"
-      redirect_to :back
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
     else
-      flash[:alert] = "Check the comment form, something went horribly wrong."
+      flash[:alert] = "Check the comment form, something went wrong."
       render root_path
     end
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
-    if current_user.id == @comment.user_id
-      if @comment.destroy
-        flash[:success] = "Comment deleted."
-        redirect_to root_path
-      else
-        flash[:alert] = "Something went wrong, comment could not be deleted."
-        render post_path(@post)
+    @comment = @post.comments.find(params[:id])
+
+    if @comment.user_id == current_user.id
+      @comment.delete
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
       end
-    else
-      flash[:alert] = "That doesn't belong to you!"
-      redirect_to root_path
     end
   end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,10 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   validates :image, :user_id, presence: true
+  validates :caption, length: { minimum: 3 }
+  validates :caption, length: { maximum: 300 }
 
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-
 
 end
